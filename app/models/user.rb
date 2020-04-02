be06bc9c0d5fff7fe426de559d3f313e8bd3625a
etienne cherri <etienne@cherri.fr>
2020-04-02 19:31:16 +0200

    devise almost

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   belongs_to :category
   enum user_type: { client: 1, member: 2 }, _scopes: false
 end
